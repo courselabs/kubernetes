937f6e0a5b2887d65ce84cbc10b51824f4da60b4
Elton Stoneman <elton@sixeyed.com>
2021-10-11 23:02:15 +0100

    Add image pull scripts

--- scripts/imageSync.ps1
+++ scripts/imageSync.ps1
@@ -1,6 +1,6 @@
 $images = $(cat images.txt)
 
-Write-Output '* Pulling images'
+Write-Output '* Syncing images'
 foreach ($tag in $images) {
     Write-Output "** Processing tag: $tag"
     & docker pull $tag
@@ -12,3 +12,13 @@ foreach ($tag in $images) {
     Write-Output "** Pushing: $newTag"
     & docker push $newTag
 }
+
+# This copies the images used in the labs to an ACR registry
+# Use for students who can't access Docker Hub
+# All `image:` references in specs need to be prefixe with `courselabs.azurecr.io/`
+
+# FOR ADMIN: to make the repo accessible
+# `az acr update --name courselabs --anonymous-pull-enabled`
+
+# And to make it private again:
+# `az acr update --name courselabs --anonymous-pull-enabled false`
